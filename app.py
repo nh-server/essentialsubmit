3e7f2955afcf07dbd0fc47b402c2c6519b761f39
Gruetzig <florianavilov@gmail.com>
2024-08-14 17:20:13 +0200

    slavery

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template
 from flask_httpauth import HTTPBasicAuth
 from configparser import ConfigParser
 import os
+import sys
+
+def get_essential_list():
+    essential_discord_names = []
+    for file in os.listdir("essentials"):
+        essential_discord_names.append(file[10:].split('.exefs')[0])
+    return essential_discord_names   
 
 if not "config.ini" in os.listdir():
     print("Configuration doesn't exist")
-    exit() 
+    sys.exit(4) 
+
 config = ConfigParser()
 config.read("config.ini")
 config_username = config["Access_Credentials"]["username"]
 config_password = config["Access_Credentials"]["password"]
 if config_password == "<password here>":
     print("Password not set (too dangerous)")
-    exit()
+    sys.exit(4)
+
+if not "essentials" in os.listdir():
+    print("Create an \"essentials\" folder")
+    sys.exit(4)
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -24,21 +36,33 @@ def verify_password(username, password):
 
 @app.route('/submit', methods=['POST'])
 def get_submission():
-    print(request.form['discordhandle'])
-    with open(f"essential_{request.form['discordhandle']}.exefs", "wb") as f:
+    print(f"essential.exefs submitted by {request.form['discordhandle']}")
+    with open(f"essentials/essential_{request.form['discordhandle']}.exefs", "wb") as f:
         f.write(request.files['file'].read())
 
     return "<p>Hi</p>"
+
+@app.route('/admin')
+@auth.login_required
+def admin():
+    return render_template('admin.html', entries=get_essential_list())
 
 @app.route('/retrieve', methods=['GET'])
 @auth.login_required
 def retrieve():
     name = request.args['username']
-    print(f"{name} is being retrieved")
-    with open(f"essential_{name}.exefs", "rb") as f:
+    print(f"{name} retrieved by {request.remote_addr}")
+    with open(f"essentials/essential_{name}.exefs", "rb") as f:
         essential = f.read()
-    make_response(essential)
     response = make_response(essential)
     response.headers.set('Content-Type', 'application/octet-stream')
     response.headers.set('Content-Disposition', 'attachment', filename=f'essential_{name}.exefs')
     return response
+
+@app.route('/delete')
+@auth.login_required
+def delete():
+    name = request.args['username']
+    print(f"{name} deleted by {request.remote_addr}")
+    os.remove(f"essentials/essential_{name}.exefs")
+    return "Deleted successfully. <a href=\"/admin\">Return to Admin Panel</a>"
