4e65c41a68b67184538d3e25ced4c0a5c3b98ffd
Gruetzig <florianavilov@gmail.com>
2024-08-14 10:24:11 +0200

    retrieve

--- README.md
+++ README.md
@@ -3,9 +3,14 @@
 to build this goddamn thingie, run make SERVER_ADDRESS=https://... \
 e.g. make SERVER_ADDRESS=https://3ds.web.page/submit (this is not an actual valid url i hope)
 
+# hostin
+
 app.py is a super small flask app that listens for post request on /submit, saves essential as essential_\<discordname\>.exefs and prints the discord username to stdout. \
+to retrieve an essential, you can use the retrieve api, which requires a password \
+you can set that password in config.ini \
+an example is provided, but needs to be renamed to config.ini obv \
 use gunicorn, e.g. gunicorn -w 1 app:app. I use venv to set stuff up: \
 python3 -m venv .venv \
 . .venv/bin/activate \
-pip install flask gunicorn \
+pip install flask gunicorn flask_httpauth \
 gunicorn -w 1 app:app
--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, request, make_response
+from flask_httpauth import HTTPBasicAuth
+from configparser import ConfigParser
+import os
+
+if not "config.ini" in os.listdir():
+    print("Configuration doesn't exist")
+    exit() 
+config = ConfigParser()
+config.read("config.ini")
+config_username = config["Access_Credentials"]["username"]
+config_password = config["Access_Credentials"]["password"]
+if config_password == "<password here>":
+    print("Password not set (too dangerous)")
+    exit()
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    if config_password == password and config_username == username:
+        return username
 
 @app.route('/submit', methods=['POST'])
 def get_submission():
@@ -10,3 +30,15 @@ def get_submission():
 
     return "<p>Hi</p>"
 
+@app.route('/retrieve', methods=['POST'])
+@auth.login_required
+def retrieve():
+    name = request.args['username']
+    print(f"{username} is being retrieved")
+    with open(f"essential_{username}.exefs", "rb") as f:
+        essential = f.read()
+    make_response(essential)
+    response = make_response(movable)
+    response.headers.set('Content-Type', 'application/octet-stream')
+    response.headers.set('Content-Disposition', 'attachment', filename=f'essential_{name}.exefs')
+    return response
